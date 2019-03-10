24c338edd4f4e1c62c5e5a0fcf9839bae228042c
Sankaranarayanan <nsnkrnryn@gmail.com>
2019-03-10 22:02:18 +0530

    Included Retrofit API calls

--- app/src/main/java/com/example/summit/MainActivity.java
+++ app/src/main/java/com/example/summit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.summit;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -13,10 +14,29 @@ import android.view.MenuItem;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.CommonStatusCodes;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+public class MainActivity extends AppCompatActivity implements Callback<String> {
 
     private CompoundButton autoFocus;
     private CompoundButton useFlash;
@@ -49,6 +69,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, RC_OCR_CAPTURE);
             }
         });
+
+
     }
 
     @Override
@@ -57,6 +79,55 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == CommonStatusCodes.SUCCESS) {
                 if (data != null) {
                     String text = data.getStringExtra(OcrCaptureActivity.TextBlockObject);
+                    Retrofit retrofit = new Retrofit.Builder()
+                            .baseUrl(RetroInterface.ENDPOINT)
+                            .addConverterFactory(ScalarsConverterFactory.create())
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .build();
+
+                    RetroInterface retif = retrofit.create(RetroInterface.class);
+                    Call<String> userCall = retif.getSummary(text);
+                    userCall.enqueue(this);
+                    String file = "mfile.txt";
+                    try {
+                        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(getApplicationContext().openFileOutput(file, Context.MODE_PRIVATE));
+                        outputStreamWriter.write(text);
+                        Toast.makeText(this, "File write successful", Toast.LENGTH_LONG).show();
+                        outputStreamWriter.close();
+                    }
+                    catch (IOException e) {
+                        Log.e("Exception", "File write failed: " + e.toString());
+                    }
+                    String ret = "";
+
+                    try {
+                        InputStream inputStream = getApplicationContext().openFileInput(file);
+
+                        if ( inputStream != null ) {
+                            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                            String receiveString = "";
+                            StringBuilder stringBuilder = new StringBuilder();
+
+                            while ( (receiveString = bufferedReader.readLine()) != null ) {
+                                stringBuilder.append(receiveString);
+                            }
+
+                            inputStream.close();
+                            ret = stringBuilder.toString();
+                            Toast.makeText(this, ret, Toast.LENGTH_SHORT).show();
+                            JSONArray arr = new JSONArray();
+                            arr.put(ret);
+
+                            Toast.makeText(this, "JSON:"+arr.toString(), Toast.LENGTH_LONG).show();
+                            //Toast.makeText(this, "JSON-OBJ:"+obj.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    catch (FileNotFoundException e) {
+                        Log.e("login activity", "File not found: " + e.toString());
+                    } catch (IOException e) {
+                        Log.e("login activity", "Can not read file: " + e.toString());
+                    }
                     statusMessage.setText(R.string.ocr_success);
 
                     Log.d(TAG, "Text read: " + text);
@@ -79,6 +150,18 @@ public class MainActivity extends AppCompatActivity {
         //getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    @Override
+    public void onResponse(Call<String> call, Response<String> response) {
+        Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, response.body(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onFailure(Call<String> call, Throwable t) {
+        Toast.makeText(this, "Failure", Toast.LENGTH_SHORT).show();
+    }
+
 
     //@Override
    /* public boolean onOptionsItemSelected(MenuItem item) {
