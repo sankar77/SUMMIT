a2912e6a64ec0e68cbd1ff61cbbe06c3e4facfb6
Sankaranarayanan <nsnkrnryn@gmail.com>
2019-03-31 16:48:07 +0530

    Added the POST annotation

--- app/src/main/java/com/example/summit/RetroInterface.java
+++ app/src/main/java/com/example/summit/RetroInterface.java
@@ -1,13 +1,18 @@
 package com.example.summit;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 public interface RetroInterface {
         String ENDPOINT = "http://arma-123.herokuapp.com";
-        @GET("/")
-        Call<String> getUser();
+        //@GET("/")
+        @Headers("Content-Type: application/json")
+        @POST("/doc")
+        //Call<String> getUser();
+        Call<String> getSumm(@Body String body);
 }
