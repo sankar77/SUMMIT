b96c0b523d2624ce9cf3fd5af89709fcb3170055
Sankaranarayanan <nsnkrnryn@gmail.com>
2019-04-01 15:09:37 +0530

    Added data format for server request

---
+++ app/src/main/java/com/example/summit/Summ1.java
@@ -0,0 +1,12 @@
+package com.example.summit;
+
+public class Summ1 {
+
+    final String doc;
+    final int id;
+
+    Summ1(String docs, int ide) {
+        this.doc = docs;
+        this.id = ide;
+    }
+}
